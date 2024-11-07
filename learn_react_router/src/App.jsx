0cb5cc3ee7afa61563b2f227b196119b9923231c
saurabhgup98 <saurabhgup98@gmail.com>
2024-11-07 13:59:42 +0530

    added router

---
+++ learn_react_router/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+
+function App() {
+
+  return (
+    <>
+      {/* <h1 className='bg-green-600 p-4'>React Router Course</h1> */}
+      
+    </>
+  )
+}
+
+export default App
